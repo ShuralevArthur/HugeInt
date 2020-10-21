3bc7581d70a2e25ed93f9e75f7c66ceebcc27418
ShuralevArthur <72319228+ShuralevArthur@users.noreply.github.com>
2020-10-22 00:09:10 +0300

    Add files via upload

---
+++ HugeInt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace HugeInt
+{
+    public static class HugeIntBase
+    {
+        static readonly ScriptEngine engine = Python.CreateEngine();
+        static readonly ScriptScope scope = engine.CreateScope();
+
+        public static dynamic Sum(String numberOne, String numberTwo)
+        {
+            String code = $"x = {numberOne} + {numberTwo}";
+            engine.Execute(code, scope);
+            return scope.GetVariable("x");
+        }
+
+        public static dynamic Diff(String numberOne, String numberTwo)
+        {
+            String code = $"x = {numberOne} - {numberTwo}";
+            engine.Execute(code, scope);
+            return scope.GetVariable("x");
+        }
+
+        public static dynamic Multiplication(String numberOne, String numberTwo)
+        {
+            String code = $"x = {numberOne} * {numberTwo}";
+            engine.Execute(code, scope);
+            return scope.GetVariable("x");
+        }
+
+        public static dynamic Division(String numberOne, String numberTwo)
+        {
+            String code = $"x = {numberOne} / {numberTwo}";
+            engine.Execute(code, scope);
+            return scope.GetVariable("x");
+        }
+    }
+}
